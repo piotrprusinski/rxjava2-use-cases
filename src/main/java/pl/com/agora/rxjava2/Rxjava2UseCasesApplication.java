5a1f57a723c8a3fda771b6fdf5a3b65ff7f0481e
pprusinski <piotr.prusinski@agora.pl>
2017-02-17 12:14:29 +0100

    init maven project

---
+++ src/main/java/pl/com/agora/rxjava2/Rxjava2UseCasesApplication.java
@@ -0,0 +1,12 @@
+package pl.com.agora.rxjava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rxjava2UseCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rxjava2UseCasesApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/com/agora/rxjava2/Rxjava2UseCasesApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.com.agora.rxjava2;
+
+import org.junit.Test;
+
+import io.reactivex.Observable;
+
+public class Rxjava2UseCasesApplicationTests {
+
+    @Test
+    public void shouldCreateSimpleObservable() {
+        //given
+        Observable.just("").subscribe();
+        //when
+        //then
+    }
+
+}
