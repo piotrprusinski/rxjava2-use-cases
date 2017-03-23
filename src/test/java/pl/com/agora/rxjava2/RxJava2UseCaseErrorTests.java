d38efaf90c3e39d7883e3fa3c4f269d14636d0ab
pprusinski <piotr.prusinski@agora.pl>
2017-03-23 08:34:38 +0100

    new

--- src/test/java/pl/com/agora/rxjava2/RxJava2UseCaseErrorTests.java
+++ src/test/java/pl/com/agora/rxjava2/RxJava2UseCaseErrorTests.java
@@ -1,70 +1,82 @@
 package pl.com.agora.rxjava2;
 
-import io.reactivex.Observable;
+import java.util.concurrent.Callable;
+
 import org.junit.Test;
 
-import java.util.concurrent.Callable;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 
 public class RxJava2UseCaseErrorTests {
 
-	private void log(String msg) {
-		System.out.println(Thread.currentThread().getName() + ": " + msg);
-	}
+    private void log(String msg) {
+        System.out.println(Thread.currentThread().getName() + ": " + msg);
+    }
 
-	private void logError(Throwable e) {
-		System.out.println(Thread.currentThread().getName() + ": error ->" + e);
-	}
+    private void logError(Throwable e) {
+        System.out.println(Thread.currentThread().getName() + ": error ->" + e);
+    }
 
-	@Test
-	public void throwException() throws Exception {
-		Observable<String> a = Observable.fromCallable(() -> {
-			throw new NullPointerException();
-		});
+    @Test
+    public void throwException() throws Exception {
+        Observable<String> objectObservable = Observable
+                .<String>fromCallable(() -> {
+                    throw new NullPointerException();
+                })
+                .onErrorResumeNext(throwable -> {
+                    return Observable.<String>error(new RuntimeException(""));
+                })
+                .doOnError(Throwable::printStackTrace);
+        Disposable subscribe = objectObservable
+                .subscribe(System.out::println);
+    }
 
-		a.subscribe(this::log, this::logError);
-	}
+    @Test
+    public void throwExceptionAndStop() throws Exception {
+        Observable<String> a = Observable.create((emitter) -> {
+            emitter.onNext("1");
+            emitter.onNext("2");
+            emitter.onNext("3");
+            emitter.onError(
+                    new NullPointerException()
+            );
+            emitter.onNext("4");
+        });
 
-	@Test
-	public void throwExceptionAndStop() throws Exception {
-		Observable<String> a = Observable.create((emitter) -> {
-			emitter.onNext("1");
-			emitter.onNext("2");
-			emitter.onNext("3");
-			emitter.onError(new NullPointerException());
-			emitter.onNext("4");
-		});
+        a.subscribe(this::log); // nie pojawi się 4
+    }
 
-		a.subscribe(this::log); // nie pojawi się 4
-	}
+    @Test
+    public void throwExceptionAndStop2() throws Exception {
+        Observable<String> a = Observable.create((emitter) -> {
+            emitter.onNext("1");
+            emitter.onNext("2");
+            emitter.onNext("3");
+            emitter.onError(new NullPointerException());
+            emitter.onNext("4");
+        });
+        a.onErrorReturn(error -> "");
+        a.subscribe(this::log); // nie pojawi się 4
+    }
 
-	@Test
-	public void throwExceptionAndStop2() throws Exception {
-		Observable<String> a = Observable.create((emitter) -> {
-			emitter.onNext("1");
-			emitter.onNext("2");
-			emitter.onNext("3");
-			emitter.onError(new NullPointerException());
-			emitter.onNext("4");
-		});
-		a.onErrorReturn(error -> "");
-		a.subscribe(this::log); // nie pojawi się 4
-	}
+    @Test
+    public void throwExceptionAndGoOn() throws Exception {
+        Observable<String> a = Observable.range(1, 4)
+                .flatMap(i -> Observable.fromCallable(ifThreeThenThrow(i))
+                        .onErrorResumeNext(throwable -> {
+                            return Observable.error(new RuntimeException(""));
+                        }));
+        a.subscribe(this::log); // pojawi sie 4
+    }
 
-	@Test
-	public void throwExceptionAndGoOn() throws Exception {
-		Observable<String> a = Observable.range(1, 4)
-				.flatMap(i -> Observable.fromCallable(ifThreeThenThrow(i)).onErrorReturn(error -> "pusty"));
-		a.subscribe(this::log); // pojawi sie 4
-	}
-
-	private Callable<String> ifThreeThenThrow(Integer i) {
-		return () -> {
-			if (i == 3) {
-				throw new NullPointerException();
-			} else {
-				return "" + i;
-			}
-		};
-	}
+    private Callable<String> ifThreeThenThrow(Integer i) {
+        return () -> {
+            if (i == 3) {
+                throw new NullPointerException();
+            } else {
+                return "" + i;
+            }
+        };
+    }
 
 }
--- src/test/java/pl/com/agora/rxjava2/Rxjava2UseCasesApplicationTests.java
+++ src/test/java/pl/com/agora/rxjava2/Rxjava2UseCasesApplicationTests.java
@@ -1,5 +1,6 @@
 package pl.com.agora.rxjava2;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
@@ -9,134 +10,141 @@ import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Uninterruptibles;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class Rxjava2UseCasesApplicationTests {
 
-	private <T> void log(T msg) {
-		System.out.println(Thread.currentThread().getName() + ": " + msg);
-	}
+    private <T> void log(T msg) {
+        System.out.println(Thread.currentThread().getName() + ": " + msg);
+    }
 
-	private void sleep(int i) {
-		Uninterruptibles.sleepUninterruptibly(i, TimeUnit.SECONDS);
-	}
+    private void sleep(int i) {
+        Uninterruptibles.sleepUninterruptibly(i, TimeUnit.SECONDS);
+    }
 
-	@Test
-	public void shouldCreateSimpleObservable() {
-		Observable.just("T").subscribe(this::log);
-	}
+    @Test
+    public void shouldCreateSimpleObservable() {
+        Observable.just("T").subscribe(this::log);
+    }
 
-	@Test
-	public void shouldCreateSimpleObservableWithMethodCreate() throws Exception {
-		Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+    @Test
+    public void shouldCreateSimpleObservableWithMethodCreate() throws Exception {
+        Observable<String> observable = Observable.create(observableEmitter -> {
+            observableEmitter.onComplete();
+            observableEmitter.onNext("T2");
+        });
 
-			@Override
-			public void subscribe(ObservableEmitter<String> observableEmitter) throws Exception {
-				observableEmitter.onNext("T2");
-			}
-		});
+        observable.subscribe(t -> log(t));
+    }
 
-		observable.subscribe(new Consumer<String>() {
+    @Test
+    public void shouldCreateSimpleObservableFromList() {
+        Observable.just(Lists.newArrayList("t1", "t2", "t3"))
+                .map(s -> {
+                    System.out.println(s);
+                    return s;
+                })
+                .subscribe(this::log);
+    }
 
-			@Override
-			public void accept(String t) throws Exception {
-				log(t);
-			}
-		});
-	}
+    @Test
+    public void shouldCreateSimpleObservableFromList2() {
+        Observable.fromIterable(Lists.newArrayList("t1", "t2", "t3")).subscribe(this::log);
+    }
 
-	@Test
-	public void shouldCreateSimpleObservableFromList() {
-		Observable.just(Lists.newArrayList("t1", "t2", "t3")).subscribe(this::log);
-	}
+    @Test
+    public void shouldRunTaskInThread() {
+        Observable.fromIterable(Lists.newArrayList("t1", "t2", "t3"))
+                .subscribeOn(Schedulers.from(ForkJoinPool.commonPool()))
+                .subscribe(this::log);
+        //sleep(1);
+    }
 
-	@Test
-	public void shouldCreateSimpleObservableFromList2() {
-		Observable.fromIterable(Lists.newArrayList("t1", "t2", "t3")).subscribe(this::log);
-	}
+    @Test
+    public void shouldMakeKebab() throws Exception {
+        Observable<String> meatObservable = Observable
+                .fromIterable(Lists.newArrayList("meat 1", "meat 2", "meat 3", "meat 4"));
+        Observable<String> breadObservable = Observable
+                .fromIterable(Lists.newArrayList("bread 1", "bread 2", "bread 3", "bread 4"));
+        Observable<String> vegetablesObservable = Observable
+                .fromIterable(Lists.newArrayList("vegetables 1", "vegetables 2", "vegetables 3"));
 
-	@Test
-	public void shouldRunTaskInThread() {
-		Observable.fromIterable(Lists.newArrayList("t1", "t2", "t3")).subscribeOn(Schedulers.single())
-				.subscribe(this::log);
-		// sleep(1);
-	}
+        Observable.zip(meatObservable,
+                breadObservable,
+                vegetablesObservable,
+                (m, b, v) -> String.format("%s, %s, %s", m, b, v))
+                .subscribe(this::log);
+    }
 
-	@Test
-	public void shouldMakeKebab() throws Exception {
-		Observable<String> meatObservable = Observable
-				.fromIterable(Lists.newArrayList("meat 1", "meat 2", "meat 3", "meat 4"));
-		Observable<String> breadObservable = Observable
-				.fromIterable(Lists.newArrayList("bread 1", "bread 2", "bread 3", "bread 4"));
-		Observable<String> vegetablesObservable = Observable
-				.fromIterable(Lists.newArrayList("vegetables 1", "vegetables 2", "vegetables 3"));
+    private final MeatRepository meatRepo = new MeatRepository();
 
-		Observable.zip(meatObservable, breadObservable, vegetablesObservable,
-				(m, b, v) -> String.format("%s, %s, %s", m, b, v)).subscribe(this::log);
-	}
+    @Test
+    public void shouldMakeKebabSync() throws Exception {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
 
-	private final MeatRepository meatRepo = new MeatRepository();
+        Observable<String> meatObservable = Observable.range(1, 3)
+                .flatMap(i -> Observable.fromCallable(() -> meatRepo.makeMeat(i)));
 
-	@Test
-	public void shouldMakeKebabSync() throws Exception {
-		StopWatch stopWatch = new StopWatch();
-		stopWatch.start();
+        Observable<String> breadObservable = Observable
+                .fromIterable(Lists.newArrayList("bread 1", "bread 2", "bread 3", "bread 4"));
+        Observable<String> vegetablesObservable = Observable
+                .fromIterable(Lists.newArrayList("vegetables 1", "vegetables 2", "vegetables 3"));
 
-		Observable<String> meatObservable = Observable.range(1, 3)
-				.flatMap(i -> Observable.fromCallable(() -> meatRepo.makeMeat(i)));
+        Observable.zip(meatObservable,
+                breadObservable,
+                vegetablesObservable,
+                (m, b, v) -> String.format("%s, %s, %s", m, b, v)).subscribe(this::log);
+        stopWatch.stop();
+        log(stopWatch.getTotalTimeSeconds() + "s");
+    }
 
-		Observable<String> breadObservable = Observable
-				.fromIterable(Lists.newArrayList("bread 1", "bread 2", "bread 3", "bread 4"));
-		Observable<String> vegetablesObservable = Observable
-				.fromIterable(Lists.newArrayList("vegetables 1", "vegetables 2", "vegetables 3"));
+    @Test
+    public void shouldMakeKebabNotWorkingAsync() throws Exception {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
 
-		Observable.zip(meatObservable, breadObservable, vegetablesObservable,
-				(m, b, v) -> String.format("%s, %s, %s", m, b, v)).subscribe(this::log);
-		stopWatch.stop();
-		log(stopWatch.getTotalTimeSeconds() + "s");
-	}
+        Observable<String> meatObservable = Observable
+                .range(1, 3)
+                .flatMap(i -> Observable.fromCallable(() -> meatRepo.makeMeat(i)))
+                .subscribeOn(Schedulers.io());
 
-	@Test
-	public void shouldMakeKebabNotWorkingAsync() throws Exception {
-		StopWatch stopWatch = new StopWatch();
-		stopWatch.start();
+        Observable<String> breadObservable = Observable
+                .fromIterable(Lists.newArrayList("bread 1", "bread 2", "bread 3", "bread 4"));
+        Observable<String> vegetablesObservable = Observable
+                .fromIterable(Lists.newArrayList("vegetables 1", "vegetables 2", "vegetables 3"));
 
-		Observable<String> meatObservable = Observable.range(1, 3)
-				.flatMap(i -> Observable.fromCallable(() -> meatRepo.makeMeat(i))).subscribeOn(Schedulers.io());
+        Observable.zip(meatObservable,
+                breadObservable,
+                vegetablesObservable,
+                (m, b, v) -> String.format("%s, %s, %s", m, b, v)).subscribe(this::log);
+        stopWatch.stop();
+        log(stopWatch.getTotalTimeSeconds() + "s");
+        sleep(5);
+    }
 
-		Observable<String> breadObservable = Observable
-				.fromIterable(Lists.newArrayList("bread 1", "bread 2", "bread 3", "bread 4"));
-		Observable<String> vegetablesObservable = Observable
-				.fromIterable(Lists.newArrayList("vegetables 1", "vegetables 2", "vegetables 3"));
+    @Test
+    public void shouldMakeKebabAsyncParaller() throws Exception {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
 
-		Observable.zip(meatObservable, breadObservable, vegetablesObservable,
-				(m, b, v) -> String.format("%s, %s, %s", m, b, v)).subscribe(this::log);
-		stopWatch.stop();
-		log(stopWatch.getTotalTimeSeconds() + "s");
-		sleep(5);
-	}
+        Observable<String> meatObservable = Observable
+                .range(1, 3)
+                .flatMap(i -> Observable.fromCallable(() -> meatRepo.makeMeat(i))
+                        .subscribeOn(Schedulers.io()));
 
-	@Test
-	public void shouldMakeKebabAsyncParaller() throws Exception {
-		StopWatch stopWatch = new StopWatch();
-		stopWatch.start();
+        Observable<String> breadObservable = Observable
+                .fromIterable(Lists.newArrayList("bread 1", "bread 2", "bread 3", "bread 4"));
+        Observable<String> vegetablesObservable = Observable
+                .fromIterable(Lists.newArrayList("vegetables 1", "vegetables 2", "vegetables 3"));
 
-		Observable<String> meatObservable = Observable.range(1, 3)
-				.flatMap(i -> Observable.fromCallable(() -> meatRepo.makeMeat(i)).subscribeOn(Schedulers.io()));
-
-		Observable<String> breadObservable = Observable
-				.fromIterable(Lists.newArrayList("bread 1", "bread 2", "bread 3", "bread 4"));
-		Observable<String> vegetablesObservable = Observable
-				.fromIterable(Lists.newArrayList("vegetables 1", "vegetables 2", "vegetables 3"));
-
-		Observable.zip(meatObservable, breadObservable, vegetablesObservable,
-				(m, b, v) -> String.format("%s, %s, %s", m, b, v)).subscribe(this::log);
-		stopWatch.stop();
-		log(stopWatch.getTotalTimeSeconds() + "s");
-		sleep(5);
-	}
+        Observable.zip(meatObservable,
+                breadObservable,
+                vegetablesObservable,
+                (m, b, v) -> String.format("%s, %s, %s", m, b, v)).subscribe(this::log);
+        stopWatch.stop();
+        log(stopWatch.getTotalTimeSeconds() + "s");
+        sleep(5);
+    }
 
 }
